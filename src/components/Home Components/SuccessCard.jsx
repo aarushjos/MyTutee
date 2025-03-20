ac1a5c4d51a93966562766de83b10835deef28bd
aarushjos <joshiaarush5@gmail.com>
2025-03-20 21:50:07 +0530

    Commit #3

--- src/components/Home Components/SuccessCard.jsx
+++ src/components/Home Components/SuccessCard.jsx
@@ -7,7 +7,7 @@ export const SuccessCard = (props) => {
     <div className="Scard">
       <ul className='ScardUl'>
         <li><img src={Quote} alt="Quote" /></li>
-        <li><h4 style={{ fontWeight: 'lighter', fontSize: "1.2rem" }}>{props.story}</h4></li>
+        <li><h4 style={{ fontWeight: 'lighter' }}>{props.story}</h4></li>
         <li className="spacer"></li> {/* This pushes the next elements to the bottom */}
         <li><h3 style={{ color: "#E3651B" }}>{props.name}</h3></li>
         <li><h5>{props.exam}</h5></li>
